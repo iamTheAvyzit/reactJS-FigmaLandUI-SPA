d108af5a6fb3fa0d524827bf739556b6c5d375b4
Abhijeet <94052679+iamTheAvyzit@users.noreply.github.com>
2022-01-06 00:42:21 -0500

    Add files via upload

---
+++ src/components/Testimonials/Testimonials.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Cards } from '../imports'
+import './Testimonials.css'
+
+export const Testimonials = () => {
+    return (
+        <div className='test'>
+            <p>Testimonials</p>
+            <img src={Cards}/>
+            <button>More Testimonials</button>
+        </div>
+    )
+}
